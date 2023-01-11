52524572a318840a77d0d497a2ef1e7e1bc72c6f
James Aidan Zimmermann <zimmermann.lowbrass@gmail.com>
2023-01-11 11:14:48 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+// const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea';
+
+
+let companyCeo = 'Susan Smith';
